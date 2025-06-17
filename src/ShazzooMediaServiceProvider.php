51475df7e5cde0804ec9203021e1ab05be3efc41
FinnWiel <finnvandewiel01@gmail.com>
2025-06-17 16:07:35 +0200

    fix: removed comment

--- src/ShazzooMediaServiceProvider.php
+++ src/ShazzooMediaServiceProvider.php
@@ -102,8 +102,6 @@ class ShazzooMediaServiceProvider extends PackageServiceProvider
             'application/x-shockwave-flash',  // .swf
         ]);
 
-        ///test
-
         // Register the MediaPolicy if it exists in the config
         if (config('shazzoo_media.media_policies')) {
             $customPolicy = app_path('Policies/MediaPolicy.php');
